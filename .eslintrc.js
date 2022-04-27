b53007959300d2fd7b9db9b61d15ff0d6a92f3a7
qiulongwen <longwen@spacesystech.com>
2022-04-27 11:11:19 +0800

    修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Author:
+ * @Date: 2022-04-27 10:32:41
+ * @LastEditTime: 2022-04-27 11:07:05
+ * @LastEditors: qiulongwen
+ * @Description:
+ * @FilePath: \dashboard_frame\.eslintrc.js
+ */
 module.exports = {
   root: true,
   env: {
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author:
+ * @Date: 2022-04-27 10:32:40
+ * @LastEditTime: 2022-04-27 11:01:47
+ * @LastEditors: qiulongwen
+ * @Description:
+ * @FilePath: \dashboard_frame\lint-staged.config.js
+ */
 module.exports = {
   "*.{js,jsx,vue}": "vue-cli-service lint",
 };
